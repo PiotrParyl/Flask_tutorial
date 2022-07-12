0d4ef25234a65f53357cf070b86de8dee4230a9c
piotr <piotrekzrydek@gmial.com>
2022-07-13 00:51:09 +0200

    lern POST method

--- Flask Tutorial Tim/flask_1.py
+++ Flask Tutorial Tim/flask_1.py
@@ -4,10 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", )
 
 
+@app.route("/test")
+def test():
+    return render_template("new.html" )
 
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
