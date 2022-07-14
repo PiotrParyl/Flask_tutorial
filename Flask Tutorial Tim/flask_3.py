6ce41b4394b2bcf45857d13be3c7a886b04f706f
piotr <piotrekzrydek@gmial.com>
2022-07-14 18:50:21 +0200

    Oke, here i lern how to use SQLAlchemi in flask

--- Flask Tutorial Tim/flask_3.py
+++ Flask Tutorial Tim/flask_3.py
@@ -1,15 +1,42 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+
+import email
+
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 
 app = Flask(__name__)
 app.secret_key = "sdasdsfgsd"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=5)
+
+
+# Database
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+
 
 @app.route("/login", methods=["POST","GET"])
 def login():
@@ -17,28 +44,46 @@ def login():
         session.permanent  = True
         user = request.form["nm"]
         session["user"] = user
+        flash( "Login succesful")
         return redirect(url_for("user"))
     else:
         if "user" in session:
+            flash ("Alredy loge in")
             return redirect(url_for("user"))
 
         return render_template("login.html")
 
 
-@app.route("/user")
+#Logowanko urzytkownika czy cos takiego 
+
+@app.route("/user", methods=['POST','GET'])
 def user():
+    email = None
+
     if "user" in session:
         user = session["user"]
-        return f"<h1>{user}</h1>"
+
+        if request.method == 'POST':
+            email= request.form["email"]
+            session["email"]=email
+            flash("Email seved")
+
+        return render_template("user.html", email=email)
     else:
+        flash("You are not loge in ")
         return redirect(url_for("login"))
+
 
 @app.route("/logout")
 def logout():
     session.pop("user",None)
+    flash("you have been logged out!")
+    session.pop("use",None)
+    session.pop("email",None)
     return redirect(url_for("login"))
 
 
 
 if __name__=="__main__":
+    db.create_all()
     app.run(debug=True)
